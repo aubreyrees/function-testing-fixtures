3dea187caa4b40835acf1a5b5ac93c46fa1ffca1
Aubrey Stark-Toller <aubrey@kleetope.net>
2023-08-10 11:04:12 +0100

    For util.py:     Improved docstrings     Tweaked range function for more consistant interface

--- src/function_test_fixtures/utils.py
+++ src/function_test_fixtures/utils.py
@@ -1,6 +1,11 @@
+"""Utility functions."""
+
 import random
 import copy
 from typing import Iterator, TypeVar, Iterable
+
+from .constants_and_types import ParameterCounts, NON_VAR_PARAM_TYPES
+from .parameter_stats import ParameterStats
 
 
 S = TypeVar('S')
@@ -11,9 +16,9 @@ def copy_seq_generator(seq: Iterable[S]) -> Iterator[S]:
     yield from (copy.copy(x) for x in seq)
 
 
-def split_distribution(n: int, /, *, x=None) -> tuple[tuple[int,int], ...]:
+def split_distribution(n: int, /, *, x: int | None=None) -> tuple[tuple[int,int], ...]:
     """
-    Provides 2-tuples where the sum of the tuple's members is 2.
+    Return 2-tuples where the sum of the tuple's members is 2.
 
     If n is 0 then (0, 0) is returned, otherwise (n, 0) and (0, n)
     is returned. Additionally if n > 1 then a third, randomly
@@ -22,11 +27,10 @@ def split_distribution(n: int, /, *, x=None) -> tuple[tuple[int,int], ...]:
     This third split may be forced using the option `x` keyword
     only parameter.
     """
-
     if n > 1:
         if x is None:
             x = random.randrange(1, n)
-        elif x =< 0 or x => n:
+        elif x <= 0 or x >= n:
             raise TypeError("`x` must be in range 1..n-1")
 
         return ((n, 0), (x, n - x), (0, n))
@@ -36,12 +40,15 @@ def split_distribution(n: int, /, *, x=None) -> tuple[tuple[int,int], ...]:
         return ((0, 0),)
 
 
-def test_low_range(start : int, stop : int) -> tuple[int,...]:
-    """ 
-    Build a 1 or 2 tuple whose first element is `start`. If an
-    integer exists that is greater than `start` and less than
-    `stop` then this is the second element.
-    """
+def test_low_range(n: int, m: int | None = None) -> tuple[int,...]:
+    """Equivlent to test_range(start, stop)[:-1] except start == stop is an error."""
+    if m is None:
+        start: int = 0
+        stop: int = n
+    else:
+        start: int = n
+        stop: int = m
+
     match stop - start:
         case 0 :
             raise TypeError("`start` must not equal `stop`")
@@ -53,12 +60,15 @@ def test_low_range(start : int, stop : int) -> tuple[int,...]:
             return (start, random.randrange(1, stop))
 
 
-def test_high_range(start : int, stop : int) -> tuple[int,...]:
-    """ 
-    Build a 1 or 2 tuple whose last element is `stop`. If an
-    integer exists that is greater than `start` and less than
-    `stop` then this is the first element.
-    """
+def test_high_range(n: int, m: int | None = None) -> tuple[int,...]:
+    """Equivlent to test_range(start, stop)[1:] except start == stop is an error."""
+    if m is None:
+        start: int = 0
+        stop: int = n
+    else:
+        start: int = n
+        stop: int = m
+
     match stop - start:
         case 0 :
             raise TypeError("`start` must not equal `stop`")
@@ -67,38 +77,39 @@ def test_high_range(start : int, stop : int) -> tuple[int,...]:
         case 2:
             return (stop-1, stop)
         case _:
-            return (random.randrange(1, stop), stop)
+            return (random.randrange(start, stop), stop)
 
 
 def test_range(n: int, m: int | None=None, /) -> tuple[int, ...]:
     """
     An immutable sequence of numbers between `start` and `stop`.
 
-    This comparable to python's builtin `range` function but rather than produce
-    every possible value between `start` and `stop` this range will have at most 
-    3 values: a min, a max and a random mid value.
+    This includes `start`, `stop` and if the the `stop - start > 1`
+    a third random int betwen start and stop.
     """
     if m is None:
-        start, stop = 0, n
+        start: int = 0
+        stop: int = n
     else:
-        start, stop = n, m
+        start: int = n
+        stop: int = m
 
-    if (stop + 2) < start:
-        return (start, random.randrange(low + 1, high), stop-1)
-    elif low < high:
-        return (start, stop-1)
-    else:
-        return (start,)
+    match stop - start:
+        case 0:
+            return (start,)
+        case 1:
+            return (start, stop)
+        case 2:
+            # avoid overhead of random if there is one possible
+            # intermedite value
+            return (start, start+1, stop)
+        case _:
+            return (start, random.randrange(start + 1, stop), stop)
 
 
 def build_parameter_counts(stats: ParameterStats) -> ParameterCounts:
-    """
-    Produce a `test_range` for each parameter type.
-
-
-    This uses low
-    """
+    """Produce a `test_range` for each parameter type."""
     return {
-        pt: helper(stats.required_counters[pt], stats.counters[pt])
+        pt: test_range(stats.required_counters[pt], stats.counters[pt])
         for pt in NON_VAR_PARAM_TYPES
     }
